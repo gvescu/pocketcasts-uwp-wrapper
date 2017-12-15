ad08a6e6e363555e18f9ab31cfdfa95781b565aa
Michael Alan <Michael@Ex-Machina.us>
2017-12-15 14:33:09 -0700

    Implemented Media Control Metadata, Bugfixes

--- PocketcastsUWP/MainPage.xaml.cs
+++ PocketcastsUWP/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Windows.Media;
 using Windows.UI.Core;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 
@@ -11,6 +14,14 @@ namespace PocketcastsUWP
 
     public sealed partial class MainPage : Page
     {
+        string status = string.Empty;
+        string playCommand = "angular.element(document).injector().get('mediaPlayer').playPause()";
+        string forwardCommand = "angular.element(document).injector().get('mediaPlayer').jumpForward()";
+        string backCommand = "angular.element(document).injector().get('mediaPlayer').jumpBack()";
+
+        string getEpisodeCommand = "angular.element(document).injector().get('mediaPlayer').episode.title.toString()";
+        string getPodcastCommand = "angular.element(document).injector().get('mediaPlayer').podcast.title.toString()";
+        string getArtCommand = "angular.element(document).injector().get('mediaPlayer').podcast.thumbnail_url.toString()";
 
         SystemMediaTransportControls systemControls;
 
@@ -23,40 +34,103 @@ namespace PocketcastsUWP
             titleBar.ForegroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);   
             titleBar.ButtonBackgroundColor = Windows.UI.Color.FromArgb(255, 244, 67, 54);
             titleBar.ButtonForegroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
-
         }
 
         async void SystemControls_ButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs args)
         {
-            string status = string.Empty;
-            string playCommand = "angular.element(document).injector().get('mediaPlayer').playPause()";
-            string statusCommand = "angular.element(document).injector().get('mediaPlayer').playing";
-            string forwardCommand = "angular.element(document).injector().get('mediaPlayer').jumpForward()";
-            string backCommand = "angular.element(document).injector().get('mediaPlayer').jumpBack()";
-
             switch (args.Button)
             {
                 case SystemMediaTransportControlsButton.Play:
                     await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
                     {
-                        await webEntryPoint.InvokeScriptAsync("eval", new string[] { playCommand });
-                        status = await webEntryPoint.InvokeScriptAsync("eval", new string[] { statusCommand });
+                        try
+                        {
+                            await webEntryPoint.InvokeScriptAsync("eval", new string[] { playCommand });
+                        }
+                        catch (Exception ex)
+                        {
+                            //Throws exception on first press if nothing is playing, not sure why
+                        }
+                        updateMediaOverlay();
                     });
-                    Debug.WriteLine("Status: " + status);
                     break;
                 case SystemMediaTransportControlsButton.Next:
                     await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
                     {
-                        await webEntryPoint.InvokeScriptAsync("eval", new string[] { forwardCommand });
+                        try {
+                            await webEntryPoint.InvokeScriptAsync("eval", new string[] { forwardCommand });
+                        }
+                        catch (Exception ex)
+                        {
+                            //Throws exception on first press if nothing is playing, not sure why
+                        }
                     });
                     break;
                 case SystemMediaTransportControlsButton.Previous:
                     await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
                     {
-                        await webEntryPoint.InvokeScriptAsync("eval", new string[] { backCommand });
+                        try {
+                            await webEntryPoint.InvokeScriptAsync("eval", new string[] { backCommand });
+                        }
+                        catch (Exception ex)
+                        {
+                            //Throws exception on first press if nothing is playing, not sure why
+                        }
                     });
+                    injectNotification();
                     break;
+
             }
+        }
+
+        async private void updateMediaOverlay()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                SystemMediaTransportControlsDisplayUpdater mediaOverlay = systemControls.DisplayUpdater;
+                mediaOverlay.Type = Windows.Media.MediaPlaybackType.Music;
+
+                String episode, podcast, artURI;
+                
+                try
+                {
+                    episode = await webEntryPoint.InvokeScriptAsync("eval", new string[] { getEpisodeCommand });
+                    podcast = await webEntryPoint.InvokeScriptAsync("eval", new string[] { getPodcastCommand });
+                    artURI = await webEntryPoint.InvokeScriptAsync("eval", new string[] { getArtCommand });                    
+                }
+                catch(Exception ex)
+                {
+                    episode = "";
+                    podcast = "";
+                    artURI = "ms-appx:///Assets/StoreLogo.scale-400.png";
+                }
+
+                mediaOverlay.MusicProperties.Artist = podcast;
+                mediaOverlay.MusicProperties.Title = episode;
+                mediaOverlay.Thumbnail = Windows.Storage.Streams.RandomAccessStreamReference.CreateFromUri(new Uri(artURI));
+
+                mediaOverlay.Update();
+            });
+
+        }
+
+        private void webEntryPoint_ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            updateMediaOverlay();
+        }
+
+        async private void injectNotification()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                string playPauseNotifyFunc = "var btns = document.getElementsByClassName('episode_button'); " +
+                                             "for (var i=0; i < btns.length; i++) { " +
+                                             "btns[i].addEventListener('click', function() { " +
+                                             "window.external.notify(\"PlayPauseEvent\"); })} ";
+
+                await Task.Delay(500); //Superrrr Ghetto, but can't figure out how to fire event on rendering complete ('NavigationCompleted" fires to soon and also multiple times for some reason)
+                await webEntryPoint.InvokeScriptAsync("eval", new string[] { playPauseNotifyFunc });                
+            });
         }
 
         private void webEntryPoint_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
@@ -69,7 +143,8 @@ namespace PocketcastsUWP
             systemControls.IsNextEnabled = true;
             systemControls.IsPreviousEnabled = true;
             systemControls.IsRewindEnabled = true;
+            injectNotification();
+            updateMediaOverlay();
         }
-
     }
 }
